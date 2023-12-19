83e797125f7cb983ea6f0cf292b6b59bbc24b465
Harshil Tomar <harshiltomar20@gmail.com>
2023-12-19 20:57:59 +0530

    1
    Initialize

---
+++ app/posts/new/page.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+// /posts/new
+const page = () => {
+  return <div>NEW POSTS</div>;
+};
+
+export default page;
---
+++ app/posts/page.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+// /post
+
+const page = () => {
+  return <div>POSTS</div>;
+};
+
+export default page;
